a1952de87af1ed2b09adeac7c3538c5b1b8ec8e9
RMPR <akoudanilo@gmail.com>
2021-01-16 00:19:00 +0100

    Read a single line from stdin
    https://stackoverflow.com/questions/30186037/how-can-i-read-a-single-line-from-stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, BufRead, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    print!("Enter something \n > ");
+    stdout.flush();
+    println!("{}", stdin.lock().lines().next().unwrap().unwrap());
+    /*
+    for line in stdin.lock().lines() {
+        println!("{}", line.unwrap());
+    }
+    */
 }
