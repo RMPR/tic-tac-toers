c6241dffc8fb40e50ae48f2e721749f1995ca7ec
Mairo Rufus <akoudanilo@gmail.com>
2021-02-26 17:33:18 +0100

    Print the current state of the board

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
 use std::io::{self, BufRead, Write};
 
+#[derive(Copy, Clone)]
+enum Mark {
+    /*
+     * States of a square
+     */
+    Empty,
+    Cross,
+    Circle,
+}
+
+fn print_board(board: [[Mark; 3]; 3]) {
+    /*
+     * Prints the current state of the board
+     */
+    for line in board.iter() {
+        for cell in line.iter() {
+            match cell {
+                Mark::Empty => print!("|   |"),
+                Mark::Cross => print!("| X |"),
+                Mark::Circle => print!("| ◯ |"),
+            }
+        }
+        println!();
+    }
+}
+
+fn verify_board(board: [[Mark; 3]; 3]) -> bool {
+    /*
+     * Verify if there's a winner
+     */
+    true
+}
+
 fn main() {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
 
-    let mut _board: [[bool; 3]; 3] = [[false; 3]; 3];
+    let mut board: [[Mark; 3]; 3] = [[Mark::Empty; 3]; 3];
+
+    print_board(board);
 
     print!("Enter something \n > ");
     let _ = stdout.flush();
